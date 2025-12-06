ac5066322731cee47ffccc392837db18e1efac1b
takogaki <92566544+takogaki@users.noreply.github.com>
2025-12-07 01:27:22 +0900

    add channels

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,95 @@
+# from django.shortcuts import render, redirect, get_object_or_404
+# from django.views import View
+# from faker import Faker
+# from .forms import CommentForm, PostForm
+# from .models import Post, Comment
+# from django.db.models import Prefetch
+
+# fake = Faker()  # Fakerインスタンス
+
+# # フロントページビュー
+# def frontpage(request):
+#     posts = Post.objects.all().order_by('-posted_date')
+#     form = PostForm()  # PostFormのインスタンス化
+
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.name = post.name or fake.name()  # 名前がなければランダムな名前を生成
+#             post.save()
+#             form.save()
+#             return redirect("blog:frontpage")  # 投稿後、frontpageにリダイレクト
+
+#     return render(request, "blog/frontpage.html", {"posts": posts, "form": form})
+
+
+# # 新しい投稿を作成するビュー
+# class PostCreateView(View):
+#     def get(self, request):
+#         form = PostForm()
+#         return render(request, "blog/frontpage.html", {"form": form})
+
+#     def post(self, request):
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.name = post.name or fake.name()  # 名前がなければランダムな名前を生成
+#             post.save()
+#             form.save()
+#             return redirect("blog:frontpage")
+#         return render(request, "blog/frontpage.html", {"form": form})
+    
+
+# def get_comment_form(parent=None):
+#     from .forms import CommentForm
+#     return CommentForm(parent=parent)
+
+
+# # 投稿の詳細ビュー
+# def post_detail(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+
+#     # 親コメントを新しい順で取得
+#     comments = Comment.objects.filter(post=post, parent__isnull=True).order_by('-posted_date')
+
+#     # 子コメントも新しい順でプリフェッチ
+#     parent_comments = parent_comments.prefetch_related(
+#         Prefetch(
+#             'replies',
+#             queryset=Comment.objects.filter(post=post).order_by('-posted_date')
+#         )
+#     )
+
+
+#     # コメント投稿処理
+#     if request.method == "POST":
+#         parent_id = request.POST.get("parent_comment")  # 親コメントID
+#         reply_to_name = request.POST.get("reply_to")  # 宛名
+#         parent_comment = Comment.objects.get(id=parent_id) if parent_id else None
+
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.post = post
+#             comment.parent = parent_comment
+#             comment.reply_to = reply_to_name  # 宛名をセット
+#             comment.save()
+#             return redirect("blog:post_detail", slug=post.slug)  # 投稿後、詳細ページをリロード
+#     else:
+#         form = CommentForm()
+
+#     return render(request, "blog/post_detail.html", {"post": post, "comments": parent_comments, "form": form})
+
+# post_create = PostCreateView.as_view()
+
+
+
+
+
+
+
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from faker import Faker
@@ -5,26 +97,25 @@ from .forms import CommentForm, PostForm
 from .models import Post, Comment
 from django.db.models import Prefetch
 
-fake = Faker()  # Fakerインスタンス
+fake = Faker()
 
-# フロントページビュー
+
 def frontpage(request):
     posts = Post.objects.all().order_by('-posted_date')
-    form = PostForm()  # PostFormのインスタンス化
+    form = PostForm()
 
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.name = post.name or fake.name()  # 名前がなければランダムな名前を生成
+            post.name = post.name or fake.name()
             post.save()
-            form.save()
-            return redirect("blog:frontpage")  # 投稿後、frontpageにリダイレクト
+            return redirect("blog:frontpage")
 
     return render(request, "blog/frontpage.html", {"posts": posts, "form": form})
 
 
-# 新しい投稿を作成するビュー
+
 class PostCreateView(View):
     def get(self, request):
         form = PostForm()
@@ -34,26 +125,29 @@ class PostCreateView(View):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.name = post.name or fake.name()  # 名前がなければランダムな名前を生成
+            post.name = post.name or fake.name()
             post.save()
-            form.save()
             return redirect("blog:frontpage")
         return render(request, "blog/frontpage.html", {"form": form})
-    
 
+
+
+# ✨ 返信フォーム生成
 def get_comment_form(parent=None):
-    from .forms import CommentForm
     return CommentForm(parent=parent)
 
 
-# 投稿の詳細ビュー
+
+# ✨ 投稿の詳細（コメント付き）
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
 
     # 親コメントを新しい順で取得
-    comments = Comment.objects.filter(post=post, parent__isnull=True).order_by('-posted_date')
+    parent_comments = Comment.objects.filter(
+        post=post, parent__isnull=True
+    ).order_by('-posted_date')
 
-    # 子コメントも新しい順でプリフェッチ
+    # 子コメントもプリフェッチ
     parent_comments = parent_comments.prefetch_related(
         Prefetch(
             'replies',
@@ -61,23 +155,31 @@ def post_detail(request, slug):
         )
     )
 
-    # コメント投稿処理
+    # コメント投稿時
     if request.method == "POST":
-        parent_id = request.POST.get("parent_comment")  # 親コメントID
-        reply_to_name = request.POST.get("reply_to")  # 宛名
-        parent_comment = Comment.objects.get(id=parent_id) if parent_id else None
+        parent_id = request.POST.get("parent_comment")
+        reply_to_name = request.POST.get("reply_to")
 
-        form = CommentForm(request.POST)
+        parent_comment = Comment.objects.get(id=parent_id) if parent_id else None
+        form = CommentForm(request.POST, parent=parent_comment)
+
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
             comment.parent = parent_comment
-            comment.reply_to = reply_to_name  # 宛名をセット
+            comment.reply_to = reply_to_name
             comment.save()
-            return redirect("blog:post_detail", slug=post.slug)  # 投稿後、詳細ページをリロード
+            return redirect("blog:post_detail", slug=post.slug)
+
     else:
         form = CommentForm()
 
-    return render(request, "blog/post_detail.html", {"post": post, "comments": parent_comments, "form": form})
+    # 親コメントごとに返信フォームを作る
+    reply_forms = {c.id: CommentForm(parent=c) for c in parent_comments}
 
-post_create = PostCreateView.as_view()
+    return render(request, "blog/post_detail.html", {
+        "post": post,
+        "parent_comments": parent_comments,
+        "form": form,
+        "reply_forms": reply_forms
+    })
