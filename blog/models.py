9897313bb6ac2d9cbf9bd76d2a56f52db9a93b92
takogaki <92566544+takogaki@users.noreply.github.com>
2025-12-08 21:22:23 +0900

    aaa

--- blog/models.py
+++ blog/models.py
@@ -1,54 +1,57 @@
-from django.db import models
-from faker import Faker
-from django.utils.text import slugify
-import unicodedata
+# from django.db import models
+# from faker import Faker
+# from django.utils.text import slugify
+# import unicodedata
 
-fake = Faker()  # Fakerのインスタンスを作成
+# fake = Faker()  # Fakerのインスタンスを作成
 
-class Post(models.Model):
-    name        = models.CharField(max_length=255, blank=True, null=True)
-    title       = models.CharField(max_length=255)
-    slug        = models.SlugField(unique=True, blank=True)
-    body        = models.TextField(verbose_name="本文")
-    posted_date = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
+# class Post(models.Model):
+#     name        = models.CharField(max_length=255, blank=True, null=True)
+#     title       = models.CharField(max_length=255)
+#     slug        = models.SlugField(unique=True, blank=True)
+#     body        = models.TextField(verbose_name="本文")
+#     posted_date = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
 
-    def save(self, *args, **kwargs):
-        # Fakerでランダムネーム
-        if not self.name:
-            self.name = fake.name()
+#     def save(self, *args, **kwargs):
+#         # Fakerでランダムネーム
+#         if not self.name:
+#             self.name = fake.name()
 
-        # slug生成
-        if not self.slug:
-            # ✨ unicodedata.normalize を使用
-            normalized_title = unicodedata.normalize('NFKD', self.title)
-            original_slug = slugify(normalized_title)
+#         # slug生成
+#         if not self.slug:
+#             # ✨ unicodedata.normalize を使用
+#             normalized_title = unicodedata.normalize('NFKD', self.title)
+#             original_slug = slugify(normalized_title)
 
-            self.slug = original_slug
+#             self.slug = original_slug
 
-            # ユニークなslugが必要な場合
-            counter = 1
-            while Post.objects.filter(slug=self.slug).exists():
-                self.slug = f"{original_slug}-{counter}"
-                counter += 1
+#             # ユニークなslugが必要な場合
+#             counter = 1
+#             while Post.objects.filter(slug=self.slug).exists():
+#                 self.slug = f"{original_slug}-{counter}"
+#                 counter += 1
 
-        super().save(*args, **kwargs)
+#         super().save(*args, **kwargs)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
 
-class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=50, null=True)
-    body = models.TextField()
-    posted_date = models.DateTimeField(auto_now_add=True)
-    parent = models.ForeignKey(
-        'self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies'
-    )
-    reply_to = models.CharField(max_length=50, null=True, blank=True)
+# class Comment(models.Model):
+#     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+#     name = models.CharField(max_length=50, null=True)
+#     body = models.TextField()
+#     posted_date = models.DateTimeField(auto_now_add=True)
+#     parent = models.ForeignKey(
+#         'self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies'
+#     )
+#     reply_to = models.CharField(max_length=50, null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.name} >>> {self.reply_to}: {self.body[:20]}"
+#     def __str__(self):
+#         return f"{self.name} >>> {self.reply_to}: {self.body[:20]}"
+
+
+
 
 
 
@@ -106,5 +109,50 @@ class Comment(models.Model):
 #     def __str__(self):
 #         return f"{self.name} >>> {self.reply_to}: {self.body[:20]}"
     
+
+
+
+from django.db import models
+from faker import Faker
+from django.utils.text import slugify
+import unicodedata
+from uuid import uuid4
+
+fake = Faker()
+
+class Post(models.Model):
+    name        = models.CharField(max_length=255, blank=True, null=True)
+    title       = models.CharField(max_length=255)
+    slug        = models.SlugField(unique=True, blank=True)
+    body        = models.TextField(verbose_name="本文")
+    posted_date = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
+
+    def save(self, *args, **kwargs):
+        # ランダムな名前
+        if not self.name:
+            self.name = fake.name()
+
+        # slug 自動生成
+        if not self.slug:
+            normalized_title = unicodedata.normalize('NFKD', self.title)
+            base_slug = slugify(normalized_title)
+
+            # ▼ 日本語タイトルのとき slugify が空になる対策
+            if not base_slug:
+                base_slug = uuid4().hex[:10]  # ランダム英数字
+
+            self.slug = base_slug
+
+            # ▼ 一意性の確保
+            counter = 1
+            while Post.objects.filter(slug=self.slug).exists():
+                self.slug = f"{base_slug}-{counter}"
+                counter += 1
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
 
 
