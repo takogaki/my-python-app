23905b278f662d964225748658bf3fc641f026fb
takogaki <92566544+takogaki@users.noreply.github.com>
2025-12-24 20:38:40 +0900

    aaa

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from django.views import generic
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.contrib import messages
 from django.conf import settings
 
 from django.utils.http import (
