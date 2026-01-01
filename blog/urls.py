118f58720bad715a10ee411f6af0add1070f3483
takogaki <92566544+takogaki@users.noreply.github.com>
2026-01-01 15:28:54 +0900

    ddd

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,6 @@ import uuid
 from django.urls import path
 from .views import frontpage
 
-urlpatterns = [
-    path("", frontpage, name="frontpage"),
-]
-
-
 def get_device_id(request):
     """
     未ログインユーザー用の一意な端末IDを返す
@@ -24,7 +19,7 @@ def get_device_id(request):
 
 app_name = "blog"
 urlpatterns = [
-    path('', views.frontpage, name="frontpage"),
+    path("", frontpage, name="frontpage"),
     path("posts/create/", views.post_create, name="post_create"),
     path('posts/<slug:slug>/', views.post_detail, name="post_detail"),
 ]
