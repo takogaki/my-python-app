6ed9b49cd20186f11df9051ec6925b33a66ab794
takogaki <92566544+takogaki@users.noreply.github.com>
2026-01-03 01:31:59 +0900

    ddd

--- myproject/settings.py
+++ myproject/settings.py
@@ -211,7 +211,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
