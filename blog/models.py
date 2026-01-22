d389267b6e8c35ae8929656ab5509ac270c6c529
takogaki <92566544+takogaki@users.noreply.github.com>
2026-01-22 21:08:54 +0900

    d

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils.text import slugify
 import unicodedata
 from uuid import uuid4
 from django.conf import settings
+from django.urls import reverse
 
 fake = Faker()
 
@@ -80,6 +81,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
 
 class Comment(models.Model):
     VIDEO_TYPE_CHOICES = [
