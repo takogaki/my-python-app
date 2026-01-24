d36f34a847a0b62072048a35b68f46a0493c7c01
takogaki <92566544+takogaki@users.noreply.github.com>
2026-01-24 19:01:15 +0900

    d

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('chat/', include('chat.urls')),
+    path("notifications/", include("notifications.urls")),
 ] 
 
 if settings.DEBUG:
