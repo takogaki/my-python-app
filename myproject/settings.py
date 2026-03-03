42ec1b7aa0d5e96a2c019687d7c6f50a2d0203d2
takogaki <92566544+takogaki@users.noreply.github.com>
2026-03-03 23:56:31 +0900

    d

--- myproject/settings.py
+++ myproject/settings.py
@@ -211,7 +211,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL")],
+            "hosts": [os.environ.get("REDIS_URL", "redis://127.0.0.1:6379")],
         },
     },
 }
