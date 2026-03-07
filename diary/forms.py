bf6467ed908606a98e985a160034cf6861512445
takogaki <92566544+takogaki@users.noreply.github.com>
2026-03-07 18:19:50 +0900

    f

--- diary/forms.py
+++ diary/forms.py
@@ -1,9 +1,33 @@
-#diary
+from django import forms
 from django.forms import ModelForm
 from .models import Page
 
-#diary
+
 class PageForm(ModelForm):
+
     class Meta:
-        model  = Page
-        fields = ["title", "body", "page_date", "picture", 'is_public']
+        model = Page
+        fields = ["title", "body", "page_date", "picture", "is_public"]
+
+        widgets = {
+            "title": forms.TextInput(attrs={
+                "class": "form-input"
+            }),
+
+            "body": forms.Textarea(attrs={
+                "class": "form-textarea"
+            }),
+
+            "page_date": forms.DateInput(attrs={
+                "type": "date",
+                "class": "form-input"
+            }),
+
+            "picture": forms.ClearableFileInput(attrs={
+                "class": "form-file"
+            }),
+
+            "is_public": forms.CheckboxInput(attrs={
+                "class": "form-check-input"
+            })
+        }
