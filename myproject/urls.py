0d08ac46cca84b968919680b948ae3b7d3ff02ea
takogaki <92566544+takogaki@users.noreply.github.com>
2026-03-12 21:07:48 +0900

    d

--- myproject/urls.py
+++ myproject/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path("notifications/", include("notifications.urls")),
 ] 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
