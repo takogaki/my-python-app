9b8b415d114458841960dab1ca116ce2474b3f49
takogaki <92566544+takogaki@users.noreply.github.com>
2026-03-15 11:40:08 +0900

    add django-redis

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,26 +1,14 @@
-"""
-ASGI config for myproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
-# myproject/asgi.py
-
 import os
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import django
-
-django.setup()  # ← ★これが超重要★
-
-import user_messages.routing  # setup後に読み込む
+import user_messages.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
