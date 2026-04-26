ffec34aaf8aeba21788f738b59b4adcd278c7a12
takogaki <92566544+takogaki@users.noreply.github.com>
2026-04-26 12:38:13 +0900

    ddsfd

--- diary/static/diary/js/diary.js
+++ diary/static/diary/js/diary.js
@@ -1,25 +1,37 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     const button = document.getElementById("like-button");
-
     if(!button) return;
+
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let cookie of cookies) {
+                cookie = cookie.trim();
+                if (cookie.startsWith(name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.slice(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    const csrftoken = getCookie('csrftoken');
 
     button.addEventListener("click", function(){
 
-        const url = button.dataset.url;
-
-        fetch(url, {
+        fetch(button.dataset.url, {
             method: "POST",
             headers: {
-                "X-CSRFToken": document.querySelector('meta[name="csrf-token"]').content
+                "X-CSRFToken": csrftoken
             }
         })
         .then(res => res.json())
         .then(data => {
-
             document.getElementById("likes-count").textContent = data.likes;
             document.getElementById("unique-users-count").textContent = data.unique_users;
-
         });
 
     });
