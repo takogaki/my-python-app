623b728db196975153eda3a9b36fc7b4c4fc440e
takogaki <92566544+takogaki@users.noreply.github.com>
2026-05-01 00:22:56 +0900

    a

--- accounts/routing.py
+++ accounts/routing.py
@@ -1,9 +1,6 @@
-# accounts/routing.py
-
 from django.urls import re_path
-from .consumers import ChatConsumer, NotificationConsumer
+from .consumers import NotificationConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<username>\w+)/$", ChatConsumer.as_asgi()),
-    re_path(r"ws/notifications/$", NotificationConsumer.as_asgi()),  # ←これ
+    re_path(r"ws/notifications/$", NotificationConsumer.as_asgi()),
 ]
