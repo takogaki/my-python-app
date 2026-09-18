bd39bd9a688839c647531e61876354357ef87438
takogaki <92566544+takogaki@users.noreply.github.com>
2026-09-18 21:34:26 +0900

    Update settings and base template

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,8 @@ from django.urls import path
 from .views import SignUpView, UserDetailView, UserListView, like_user
 from . import views
 from .views import mypage
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 
 app_name = "accounts"
 
@@ -76,4 +78,51 @@ urlpatterns = [
     # タグでユーザーを絞り込む
     path("tag/<int:tag_id>/", views.users_by_tag, name="users_by_tag"),
     path("tag-users/", views.tag_match_users, name="tag_match_users"),
+
+        # =========================
+    # パスワードリセット
+    # =========================
+
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset.html",
+            email_template_name="accounts/password_reset_email.html",
+            subject_template_name="accounts/password_reset_subject.txt",
+            success_url=reverse_lazy(
+                "accounts:password_reset_done"
+            ),
+        ),
+        name="password_reset",
+    ),
+
+    # メール送信完了
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+
+    # 新しいパスワード設定
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html",
+            success_url=reverse_lazy(
+                "accounts:password_reset_complete"
+            ),
+        ),
+        name="password_reset_confirm",
+    ),
+
+    # パスワード変更完了
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
